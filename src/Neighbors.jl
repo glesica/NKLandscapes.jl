044fd43bb0e3dcf92907b69704e3496c812095cc
George Lesica <george@lesica.com>
2015-08-18 08:30:15 -0600

    Refactor to return matrices of neighbors for simplicity.

--- src/Neighbors.jl
+++ src/Neighbors.jl
@@ -1,4 +1,4 @@
-export neighbors, random_neighbor, neutral_neighbors, better_neighbors, best_neighbors, best_neighbor
+export neighbors, number_neighbors, random_neighbor, neutral_neighbors, better_neighbors, best_neighbors, best_neighbor
 
 function neighbors(g::Genome, ls::Landscape, muts::Int64)
   # TODO: Implement multiple mutations.
@@ -25,69 +25,84 @@ end
 
 neighbors(g::Genome, ls::Landscape) = neighbors(g, ls, 1)
 
+function number_neighbors(g::Genome, ls::Landscape, muts::Int64)
+  # TODO: Implement multiple mutations
+  if length(g) != ls.n
+    error("genome is of wrong size")
+  end
+  if length(g) < muts
+    error("gene count is less than mutation count")
+  end
+  ls.a * (ls.n - 1)
+end
+
+number_neighbors(g::Genome, ls::Landscape) = number_neighbors(g, ls, 1)
+
 function random_neighbor(g::Genome, ls::Landscape, muts::Int64)
-  # TODO: We can speed this up by jumping right to the one we chose.
-  choices = (ls.n - 1) ^ muts
+  # TODO: Speed this up by jumping directly to the mutant we chose.
+  choices = number_neighbors(g, ls, muts)
   k = rand(1:choices)
   i = 1
-  Task(function ()
-    for nbr = neighbors(g, ls, muts)
-      if i == k
-        produce(nbr)
-      end
-      i += 1
+  for nbr = neighbors(g, ls, muts)
+    if i == k
+      return nbr
     end
-  end) |> consume
+    i += 1
+  end
 end
 
 random_neighbor(g::Genome, ls::Landscape) = random_neighbor(g, ls, 1)
 
 function neutral_neighbors(g::Genome, ls::Landscape, muts::Int64)
+  # TODO: If muts > 1 or N is large this could be a memory burden.
   f0 = fitness(g, ls)
-  Task(function ()
-    for nbr = neighbors(g, ls, muts)
-      f1 = fitness(nbr, ls)
-      # TODO: Consider doing an approximate comparison here.
-      if f1 == f0
-        produce(nbr)
-      end
+  count = number_neighbors(g, ls, muts)
+  nbrs = zeros(Int64, ls.n, count) # Columns are genotypes
+  neutrals = zeros(Bool, count)
+  i = 1
+  for nbr = neighbors(g, ls, muts)
+    nbrs[:,i] = nbr
+    if fitness(nbr, ls) == f0
+      neutrals[i] = true
     end
-  end)
+    i += 1
+  end
+  nbrs[:,neutrals]
 end
 
 neutral_neighbors(g::Genome, ls::Landscape) = neutral_neighbors(g, ls, 1)
 
 function better_neighbors(g::Genome, ls::Landscape, muts::Int64)
+  # TODO: If muts > 1 or N is large this could be a memory burden.
   f0 = fitness(g, ls)
-  Task(function ()
-    for nbr = neighbors(g, ls, muts)
-      f1 = fitness(nbr, ls)
-      if f1 > f0
-        produce(nbr)
-      end
+  count = number_neighbors(g, ls, muts)
+  nbrs = zeros(Int64, ls.n, count) # Columns are genotypes
+  betters = zeros(Bool, count)
+  i = 1
+  for nbr = neighbors(g, ls, muts)
+    nbrs[:,i] = nbr
+    if fitness(nbr, ls) > f0
+      betters[i] = true
     end
-  end)
+    i += 1
+  end
+  nbrs[:,betters]
 end
 
 better_neighbors(g::Genome, ls::Landscape) = better_neighbors(g, ls, 1)
 
 function best_neighbors(g::Genome, ls::Landscape, count::Int64, muts::Int64)
-  best = zeros(Int64, count, ls.n)
+  best = zeros(Int64, ls.n, count)
   fits = zeros(Float64, count)
   for nbr = neighbors(g, ls, muts)
     f0, i = findmin(fits)
     f1 = fitness(nbr, ls)
     if f1 > f0
-      best[i,:] = nbr
+      best[:,i] = nbr
       fits[i] = f1
     end
   end
-  # Return them using a generator to keep the interface consistent.
-  Task(function ()
-    for i = sortperm(fits)
-      produce(best[i,:] |> vec)
-    end
-  end)
+  best[:,sortperm(fits)]
 end
 
 best_neighbors(g::Genome, ls::Landscape, count::Int64) = best_neighbors(g, ls, count, 1)

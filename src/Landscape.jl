433b44476893a81462fc2cfc94a385a9b4155dbe
George Lesica <george@lesica.com>
2015-08-20 23:12:30 -0600

    Add nearest-neighbor landscapes.

--- src/Landscape.jl
+++ src/Landscape.jl
@@ -11,15 +11,30 @@ type Landscape
   contribs::Array{Float64}
 end
 
-function Landscape(n::Int64, k::Int64, p::Float64, a::Int64)
+function Landscape(n::Int64, k::Int64, p::Float64, a::Int64, nearest::Bool=false)
   if k >= n
     error("k must be strictly less than n")
   end
   links = zeros(Int64, n, k + 1)
-  for i = 1:n
-    links[i,:] = [i, sample(setdiff(1:n, [i]), k, replace=false) |> sort]
+  if nearest
+    if isodd(k)
+      error("k must be even to use nearest-neighbor interactions")
+    end
+    ks = div(k, 2)
+    for i = 1:n
+      lr = links[i,:]
+      lr = [j for j = (i - ks):(i + ks)]
+      # Periodic boundary.
+      lr[lr .< 1] = lr[lr .< 1] + n
+      lr[lr .> n] = lr[lr .> n] - n
+      links[i,:] = lr
+    end
+  else
+    for i = 1:n
+      links[i,:] = [i, sample(setdiff(1:n, [i]), k, replace=false) |> sort]
+    end
   end
-  contribs = rand(Float64, n, (ones(Int, k + 1) * a)...)
+  contribs = rand(Float64, n, (ones(Int64, k + 1) * a)...)
   # Zero contributions with probability p.
   contribs[rand(Float64, size(contribs)...) .< p] = 0.0
   return Landscape(n, k, a, p, links, contribs)
@@ -28,4 +43,7 @@ end
 Landscape(n::Int64, k::Int64) = Landscape(n, k, 0.0, 2)
 Landscape(n::Int64, k::Int64, p::Float64) = Landscape(n, k, p, 2)
 Landscape(n::Int64, k::Int64, a::Int64) = Landscape(n, k, 0.0, a)
+Landscape(n::Int64, k::Int64, near::Bool) = Landscape(n, k, 0.0, 2, near)
+Landscape(n::Int64, k::Int64, p::Float64, near::Bool) = Landscape(n, k, p, 2, near)
+Landscape(n::Int64, k::Int64, a::Int64, near::Bool) = Landscape(n, k, 0.0, a, near)
 

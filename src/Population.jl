479fcc1bea95546af5e050615a9c880e264f265f
Alden Wright <github@fastmail.us>
2016-01-13 16:58:29 -0700

    Added function all_genotypes(::Type{Population}, ls::Landscape ) which creates a population of all possible genotypes for the landscape. Also added a helper function int_to_genotype( N, i ) which conversts an integer via its bitwise representation to a genotype of length N.

--- src/Population.jl
+++ src/Population.jl
@@ -1,6 +1,6 @@
 import Base.Random: rand, zeros
 
-export Population, popsize, popfits
+export Population, popsize, popfits, all_genotypes
 
 typealias Population Matrix{Int64}
 
@@ -20,6 +20,19 @@ function zeros(::Type{Population}, ls::Landscape, count::Int64)
   p
 end
 
+# Returns a population of all of the possible genotypes for the landscape.
+# Requires that the arity ls.a be 2.
+# This creates a matrix whose number of columns is exponential in the landscape length ls.n,
+#   so it should only run for moderate values of ls.n.
+function all_genotypes(::Type{Population}, ls::Landscape )
+    @assert  ls.a == 2 "Expected ls.a == 2.  The landscape arity must be 2"
+    p = zeros(Int64, ls.n, 2^ls.n)
+    for i = 0:(2^ls.n-1)
+        p[:,i+1] = int_to_genotype(ls.n,i)
+    end
+    return p
+end
+
 popsize(p::Population) = size(p)[2]
 
 # Return a vector of fitness values where the ith element in the vector
@@ -33,3 +46,20 @@ function popfits(p::Population, ls::Landscape)
   fs
 end
 
+
+# Convert Int64 to a genotype
+# len is the length of the desired genotype
+# i is the Int64 to be converted
+function int_to_genotype( N, i )
+    A = []
+    for j = 1:N
+        #println("i:",i)
+        if i & 1 != 0
+            push!(A,2)
+        else
+            push!(A,1)
+        end
+        i >>>= 1
+    end
+    return A
+end

a8732c928493c42e0fba33a316dd96ef5c5ecdcf
George Lesica <george.lesica@workiva.com>
2016-02-17 21:53:34 -0700

    Improve dockerfile with a version parameter.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM ubuntu:14.04
 
+ARG version=releases
+
 RUN apt-get -y update && \
     apt-get -y install software-properties-common && \
-    apt-add-repository ppa:staticfloat/juliareleases && \
+    apt-add-repository ppa:staticfloat/julia${version} && \
     apt-add-repository ppa:staticfloat/julia-deps && \
     apt-get -y update && \
     apt-get -y install git julia
--- runtests-docker.sh
+++ runtests-docker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-docker build -f Dockerfile-release -t julia-release .
-docker build -f Dockerfile-latest -t julia-latest .
+docker build --build-arg version=releases -t julia-release .
+docker build --build-arg version=nightlies -t julia-nightly .
 
 echo "Running tests on release version..."
 docker run -v "$PWD":/opt/src julia-release
 echo "Running tests on latest version..."
-docker run -v "$PWD":/opt/src julia-latest
+docker run -v "$PWD":/opt/src julia-nightly

9ed797beb661aec814fd2b675e433467d7cda40e
George Lesica <george.lesica@workiva.com>
2016-02-17 22:01:42 -0700

    Use docker tags for images.

--- runtests-docker.sh
+++ runtests-docker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-docker build --build-arg version=releases -t julia-release .
-docker build --build-arg version=nightlies -t julia-nightly .
+docker build --build-arg version=releases -t julia:release .
+docker build --build-arg version=nightlies -t julia:nightly .
 
 echo "Running tests on release version..."
-docker run -v "$PWD":/opt/src julia-release
+docker run -v "$PWD":/opt/src julia:release
 echo "Running tests on latest version..."
-docker run -v "$PWD":/opt/src julia-nightly
+docker run -v "$PWD":/opt/src julia:nightly

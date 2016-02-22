fb42c01006f194591f00dc3250e47832285ddc7e
George Lesica <george@lesica.com>
2016-02-22 00:43:25 -0700

    Add additional neighbor tests.

--- test/neighbors.jl
+++ test/neighbors.jl
@@ -23,6 +23,7 @@ facts("neighbors.jl") do
     ]
 
     g = NK.Genotype(0b0000, ls)
+    actual = [0b0001, 0b0010, 0b0100, 0b1000]
 
     context("$(ls)") do
       context("number_neighbors(...)") do
@@ -31,9 +32,21 @@ facts("neighbors.jl") do
 
       context("all_neighbors(...)") do
         nbrs = NK.all_neighbors(g)
+        @fact length(nbrs) --> 4
+        @fact nbrs[1].alleles --> anyof(actual...)
+        @fact nbrs[1].landscape --> ls
+        @fact nbrs[2].alleles --> anyof(actual...)
+        @fact nbrs[2].landscape --> ls
+        @fact nbrs[3].alleles --> anyof(actual...)
+        @fact nbrs[3].landscape --> ls
+        @fact nbrs[4].alleles --> anyof(actual...)
+        @fact nbrs[4].landscape --> ls
       end
 
       context("random_neighbor(...)") do
+        rnbr = NK.random_neighbor(g)
+        @fact rnbr.alleles --> anyof(actual...)
+        @fact rnbr.landscape --> ls
       end
 
       context("neutral_neighbors(...)") do
@@ -46,9 +59,18 @@ facts("neighbors.jl") do
       end
 
       context("fittest_neighbors(...)") do
+        nbrs = NK.fitter_neighbors(g, 2)
+        @fact length(nbrs) --> 2
+        @fact nbrs[1].alleles --> anyof(actual...)
+        @fact nbrs[1].landscape --> ls
+        @fact nbrs[2].alleles --> anyof(actual...)
+        @fact nbrs[2].landscape --> ls
       end
 
       context("fittest_neighbor(...)") do
+        fnbr = NK.fittest_neighbor(g)
+        @fact fnbr.alleles --> anyof(actual...)
+        @fact fnbr.landscape --> ls
       end
     end
   end

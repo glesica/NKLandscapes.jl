16595a020be4ef63b2d5b2c1b7f5e858b5d233a8
George Lesica <george@lesica.com>
2016-02-27 16:44:30 -0700

    Update mutation to use new genotypes and populations.

--- src/Mutation.jl
+++ src/Mutation.jl
@@ -2,66 +2,86 @@ import Base.Random: rand
 
 export bwmutate, bwmutate!, bsmutate, bsmutate!
 
-mutlocus(v::Int64, ls::Landscape) = setdiff(1:ls.a, v) |> rand
-
 @doc """bwmutate(p::Population, ls::Landscape, mutprob::Float64)
 
-Return a new population such that each locus of the original
+Return a new population such that each locus of each individual in the original
 population has been mutated with probability `mutprob`.
 """
-function bwmutate(p::Population, ls::Landscape, mutprob::Float64)
-  np = copy(p)
-  bwmutate!(np, ls, mutprob)
+function bwmutate(p::Population, mutprob::Float64)
+  np = deepcopy(p)
+  bwmutate!(np, mutprob)
   return np
 end
 
 @doc """bwmutate(p::MetaPopulation, ls::Landscape, mutprob::Float64)
 """
-function bwmutate(p::MetaPopulation, ls::Landscape, mutprob::Float64)
-  np = copy(p)
-  bwmutate!(np, ls, mutprob)
+function bwmutate(p::MetaPopulation, mutprob::Float64)
+  np = deepcopy(p)
+  bwmutate!(np, mutprob)
   return np
+end
+
+@doc """bwmutate!(g::Genotype, mutprob::Float64)
+
+Mutate the genotype in-place by mutating each locus with probability `mutprob`.
+"""
+function bwmutate!(g::Genotype, mutprob::Float64)
+  mask = AlleleMask(0)
+  current = AlleleMask(1)
+  for _ = 1:g.landscape.n
+    if rand() < mutprob
+      mask = mask | current
+    end
+    current = current << 1
+  end
+  g.alleles = g.alleles $ mask
 end
 
 @doc """bwmutate!(p::Population, ls::Landscape, mutprob::Float64)
 
-Mutate the population in-place by mutating each locus with probability
-`mutprob`.
+Mutate the population in-place by mutating each locus of each individual in the
+population with probability `mutprob`.
 """
-function bwmutate!(p::Population, ls::Landscape, mutprob::Float64)
-  # Use the fact that matrices can be iterated and indexed directly.
-  for i = 1:length(p)
-    if rand() < mutprob
-      p[i] = mutlocus(p[i], ls)
-    end
+function bwmutate!(p::Population, mutprob::Float64)
+  for g = p.genotypes
+    bwmutate!(g, mutprob)
   end
 end
 
-@doc """bwmutate!(p::MetaPopulation, ls::Landscape, mutprob::Float64)
+@doc """bwmutate!(mp::MetaPopulation, ls::Landscape, mutprob::Float64)
 """
-function bwmutate!(p::MetaPopulation, ls::Landscape, mutprob::Float64)
-  for ip = 1:popct(p)
-    bwmutate!(p[:,:,ip], ls, mutprob)
+function bwmutate!(mp::MetaPopulation, mutprob::Float64)
+  for p = mp.populations
+    bwmutate!(p, mutprob)
   end
 end
 
-@doc """bsmutate(p::Population, ls::Landscape)
+@doc """bsmutate(p::Population, mutprob::Float64)
 
-Return a new population with a single locus of each bitstring in the original
+Return a new population with a single locus of each individual in the original
 population mutated (bit flipped if `ls.a == 2`) with probability `mutprob`.
 """
-function bsmutate(p::Population, ls::Landscape, mutprob::Float64)
-  np = copy(p)
-  bsmutate!(np, ls, mutprob)
+function bsmutate(p::Population, mutprob::Float64)
+  np = deepcopy(p)
+  bsmutate!(np, mutprob)
   return np
 end
 
-@doc """bsmutate(p::MetaPopulation, ls::Landscape, mutprob::Float64)
+@doc """bsmutate(mp::MetaPopulation, mutprob::Float64)
 """
-function bsmutate(p::MetaPopulation, ls::Landscape, mutprob::Float64)
-  np = copy(p)
-  bsmutate!(np, ls, mutprob)
+function bsmutate(mp::MetaPopulation, mutprob::Float64)
+  np = deepcopy(p)
+  bsmutate!(np, mutprob)
   return np
+end
+
+@doc """bsmutate!(g::Genotype)
+
+Mutate a single locus within the given genotype.
+"""
+function bsmutate!(g::Genotype)
+  mask = AlleleMask(1) << rand(0:(g.landscape.n - 1))
+  g.alleles = g.alleles $ mask
 end
 
 @doc """bsmutate(p::Population, ls::Landscape)
@@ -69,20 +89,19 @@ end
 Mutate a population in-place by mutating up to one locus of each bitstring in
 the population.
 """
-function bsmutate!(p::Population, ls::Landscape, mutprob::Float64)
-  for i = 1:popsize(p)
+function bsmutate!(p::Population, mutprob::Float64)
+  for g = p.genotypes
     if rand() < mutprob
-      j = rand(1:ls.n)
-      p[j,i] = mutlocus(p[j,i], ls)
+      bsmutate!(g)
     end
   end
 end
 
 @doc """bsmutate!(p::MetaPopulation, ls::Landscape, mutprob::Float64)
 """
-function bsmutate!(p::MetaPopulation, ls::Landscape, mutprob::Float64)
-  for ip = 1:popct(p)
-    bsmutate!(p[:,:,ip], ls, mutprob)
+function bsmutate!(mp::MetaPopulation, mutprob::Float64)
+  for p = mp.populations
+    bsmutate!(p, mutprob)
   end
 end
 

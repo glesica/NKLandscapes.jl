027c95ec74fb3952889fbc529fc0e6b5309d3d08
Alden Wright <github@fastmail.us>
2016-03-08 14:55:09 -0700

    This commit saves the changes to example/peak_paths.jl.

--- example/peak_paths.jl
+++ example/peak_paths.jl
@@ -239,3 +239,31 @@ function print_paths_summary(n,k,num_landscapes)
   @printf("avg_ave_cost:%6.3f std_dev_ave_cost:%6.4f\n",sum_ave_cost/num_landscapes,std_dev_ave_cost)
   @printf("avg_ave_dec_fitness:%6.3f std_dev_ave_dec_fitness:%6.4f\n",sum_ave_dec_fitness/num_landscapes,std_dev_ave_dec_fitness)
 end
+
+@doc """ function test_paths(ls::NKLandscape, fits::Vector{Float64})
+
+Tests function least_cost_paths() by computing paths between peaks in both
+directions using a symmetric edge_cost function, and checking that lengths
+and costs are equal.
+"""
+function test_paths(ls::NKLandscape, fits::Vector{Float64})
+  bcc = sort(basinlists!(basins(ls,fits),ls),lt=(x,y)->x.peak_fitness<y.peak_fitness)
+  if length(bcc) > 1
+    for i = 1:length(bcc)
+      gi = bcc[i].gtype
+      gi_set = Set{Genotype}([gi])
+      for j = i+1:length(bcc)
+        gj = bcc[j].gtype
+        gj_set = Set{Genotype}([gj])
+        spw_ij = least_cost_paths(gi,gj_set,ls,fits,single_path=true,edge_cost_funct=edge_cost_sym)
+        spw_ji = least_cost_paths(gj,gi_set,ls,fits,single_path=true,edge_cost_funct=edge_cost_sym)
+        @test  length(spw_ij[1].path) == length(spw_ji[1].path)
+        @test_approx_eq spw_ij[1].cost spw_ji[1].cost
+      end
+    end
+  end
+end
+
+ls = NKLandscape(5,3)
+fits = lsfits(ls)
+test_paths(ls,fits)

cccf41f6cbe77a36d1a34320e5dd37116a51ebb0
Alden Wright <github@fastmail.us>
2016-03-14 10:54:55 -0600

    Substantial revision and cleanup of  src/basins.jl and example/peak_paths.jl.

--- example/peak_paths.jl
+++ example/peak_paths.jl
@@ -10,17 +10,29 @@ steps of the path.
 The cost of a path is the sum of the edge costs of the edges that make up the path.  The edge cost
 can be computed in different ways:  two examples are given below.
 
+References:  Gavrilets, Sergey, and Janko Gravner.  1997. doi: 10.1006/jtbi.1996.0242.
+Gravner, Janko, Damien Pitman, and Sergey Gavrilets. 2007.  doi: 10.1016/j.jtbi.2007.07.009.
+
 TODO:  This introductory summary could be improved.
+
+To test the functions in this file, do:
+fitdiff_weight = 8.0   # Weight on fitness differences as opposed to the number of mutational steps.
+ls = NKLandscape(5,3)
+fits = lsfits(ls)
+test_paths(ls,fits,fitdiff_weight)
+
+To print a summary of the paths between peaks for N=8, K=2 NK Landscapes, do:
+fitdiff_weight = 8.0   # Weight on fitness differences as opposed to the number of mutational steps.
+n = 8  # N for the NK landscape
+k = 2  # K for the NK landscape
+num_landscapes = 5
+print_paths_summary(n, k, num_landscapes, fitdiff_weight)
 =#
 export edge_cost_exp, edge_cost_sym, least_cost_paths, PathWintCost, LandscapePathSummary, summary_landscape_paths
 using Base.Collections
 using Base.Test
 using DataStructures
 using NKLandscapes
-
-# Two important parameters:
-fit_diff_weight = 8.0  # Weight on fitness differences as opposed to mutational steps.
-dist_cutoff = 4    # Only paths between pairs of peaks closer to each other than this cutoff are considered
 
 @doc """ type Path
 
@@ -40,7 +52,7 @@ The exponential of a cost of a path edge mulitiplied by landscape length and par
 Nonzero only when the fitess differens from gtype1 to gtype2 is negative.
 Note the dependence of the cost on the constant "fit_diff_weight".
 """
-function edge_cost_exp(gtype1::AlleleString, gtype2::AlleleString, ls::NKLandscape, fits::Vector{Float64})
+function edge_cost_exp(gtype1::AlleleString, gtype2::AlleleString, ls::NKLandscape, fits::Vector{Float64}, fit_diff_weight::Float64)
   fit_diff = (exp(max(fits[gtype2+1]-fits[gtype1+1],0.0))-exp(0.0))*ls.n*fit_diff_weight
   return 1.0 + fit_diff
 end
@@ -49,7 +61,7 @@ end
 
 A symmtric (in both directions) edge cost used for testing the correctness of function least_cost_paths().
 """
-function edge_cost_sym(gtype1::AlleleString, gtype2::AlleleString, ls::NKLandscape, fits::Vector{Float64})
+function edge_cost_sym(gtype1::AlleleString, gtype2::AlleleString, ls::NKLandscape, fits::Vector{Float64}, fit_diff_weight::Float64)
   fit_diff = abs(fits[gtype2+1]-fits[gtype1+1])*ls.n*fit_diff_weight
   return 1.0 + fit_diff
 end
@@ -68,13 +80,14 @@ QueueNode(cur::Int64, prev::Int64, cost::Float64) = QueueNode( convert(AlleleStr
 @doc """ function least_cost_paths(source::Genotype, destinations::Set, ls::NKLandscape, fits::Vector{Float64}; single_path=false)
 
 Find least cost paths from genotype "source" to the genotypes in the set "destinations" using Dijkstra's algorithm.
-"Least cost" is defined by the function "edge_cost_funct".
+"Least cost" is defined by the function "edge_cost_funct" which uses the parameter "fit_diff_weight" to control the relative
+contribution of path length and fitness decrease.
 Returns a list of paths.
 If  single_path==true, returns the first path found
  TODO:  Rename variables to be more meaningful.
 """
-function least_cost_paths(source::Genotype, destinations::Set{Genotype}, ls::NKLandscape, fits::Vector{Float64}; 
-    edge_cost_funct=edge_cost_exp, single_path=false)
+function least_cost_paths(source::Genotype, destinations::Set{Genotype}, ls::NKLandscape, fits::Vector{Float64},
+    fit_diff_weight::Float64; edge_cost_funct=edge_cost_exp, single_path=false)
   all_paths = Path[]
   destination_alleles = Set{AlleleString}([s.alleles for s in destinations])
   closed = Set()
@@ -82,35 +95,34 @@ function least_cost_paths(source::Genotype, destinations::Set{Genotype}, ls::NKL
   source_node = QueueNode(source.alleles,cons(source.alleles,nil()),0.0)
   queue[source_node] =0.0
   while !isempty(queue)
-    ng_node = Base.Collections.dequeue!(queue)
-    if ng_node.current in destination_alleles
-      setdiff!(destination_alleles, ng_node.current)
-      len = length(ng_node.previous)
+    queue_node = Base.Collections.dequeue!(queue)
+    if queue_node.current in destination_alleles
+      setdiff!(destination_alleles, queue_node.current)
+      len = length(queue_node.previous)
       path = zeros(AlleleString,len)
       min_fitness = 1.0  # maximum possible fitness
       i = len
-      for g in ng_node.previous
+      for g in queue_node.previous
         fg = fits[g+1]
-        #println("g:",g,"  fg:",fg)
         min_fitness = min(fg, min_fitness)
         path[i] = g
         i -= 1
       end
       decrease_fitness = fits[path[1]+1] - min_fitness
-      push!(all_paths,Path(path,ng_node.cost,decrease_fitness))
+      push!(all_paths,Path(path,queue_node.cost,decrease_fitness))
       if single_path
         return all_paths
       end
     end
-    if !in(ng_node.current, closed)
-      push!(closed, ng_node.current)
-      for nbr = neighbors(Genotype(ng_node.current,ls))
+    if !in(queue_node.current, closed)
+      push!(closed, queue_node.current)
+      for nbr = neighbors(Genotype(queue_node.current,ls))
         #heuristic = count_ones( nbr $ peak2 )  # can be used to convert algorithm to A* for single paths
-        new_cost = edge_cost_funct(nbr.alleles,ng_node.current,ls,fits)
+        new_cost = edge_cost_funct(nbr.alleles,queue_node.current,ls,fits,fit_diff_weight)
         value = get(queue,nbr,Void)
         if (value == Void) || (value < new_cost)
-          nbr_node = QueueNode(nbr.alleles,cons(nbr.alleles,ng_node.previous), new_cost + ng_node.cost)
-          queue[nbr_node] = new_cost + ng_node.cost
+          nbr_node = QueueNode(nbr.alleles,cons(nbr.alleles,queue_node.previous), new_cost + queue_node.cost)
+          queue[nbr_node] = new_cost + queue_node.cost
         end
       end
     end
@@ -122,7 +134,7 @@ end
   An independent computation of the fit-diff cost of a path.
   Mostly for debugging purposes.
 """
-function path_cost(path::Array{AlleleString,1},ls::NKLandscape)
+function path_cost(path::Array{AlleleString,1},ls::NKLandscape, fit_diff_weight::Float64)
   cost = 0.0
   gprev = path[1]
   for gnext in path[2:end]
@@ -152,14 +164,14 @@ type LandscapePathSummary
   denom::Int64
 end
 
-@doc """ function summary_landscape_paths(n::Int64, k::Int64; dist_cutoff::Int64=1000)
+@doc """ function summary_landscape_paths(n::Int64, k::Int64; peak_dist_cutoff::Int64=1000)
 
 Generates an NK landscape with N=n, K=k, and computes all least cost paths for all pairs of
 peaks where the first peak is less than the second and where the distance between the peaks
-is less than or equal to "dist_cutoff".
+is less than or equal to "peak_dist_cutoff".
 Returns a LandscapePathSummary object which summarizes the statistics of these paths.
 """
-function summary_landscape_paths(n::Int64, k::Int64; dist_cutoff::Int64=1000)
+function summary_landscape_paths(n::Int64, k::Int64, fit_diff_weight::Float64; peak_dist_cutoff::Int64=1000)
   if k == 0
     error("k must be greater than or equal to 0 in function summary_landscape_paths")i
   end
@@ -182,8 +194,8 @@ function summary_landscape_paths(n::Int64, k::Int64; dist_cutoff::Int64=1000)
   for i = 1:length(bcc)
     gi = bcc[i].gtype
     setdiff!(g_set,[gi])
-    filtered_set = filter(x->hamming_dist(x,gi)<dist_cutoff,g_set)
-    spw = least_cost_paths(gi,filtered_set,ls,fts)
+    filtered_set = filter(x->hamming_dist(x,gi)<peak_dist_cutoff, g_set)
+    spw = least_cost_paths(gi, filtered_set, ls, fts, fit_diff_weight)
     for sp in spw   # for each least-cost path
       ave_length += length(sp.path)-1
       ave_cost += sp.cost
@@ -204,12 +216,13 @@ end
 
 For each of "num_landscapes" landscapes, finds least-cost paths from each peak to all peaks of higher fitness.
 Results are printed for each landscape, and summarized for all landscapes.
-Processing of landscapes is distributed across processes, so this function will run faster with multiple processes.
+Processing of landscapes is distributed across processes by using pmap, 
+   so this function will run faster with multiple processes.
 """
-function print_paths_summary(n, k, num_landscapes )
+function print_paths_summary(n, k, num_landscapes, fit_diff_weight::Float64; peak_dist_cutoff::Int64=1000)
   @assert num_landscapes > 1
   println("fit diff weight: ",fit_diff_weight)
-  println("dist_cutoff: ",dist_cutoff)
+  println("peak_dist_cutoff: ",peak_dist_cutoff)
   @printf("   n\t   k\tlen_bcc\tmin_len\tave_len\tave_cst\tave_dec\n")
   sum_ave_length = 0.0
   sum_min_length = 0.0
@@ -219,7 +232,8 @@ function print_paths_summary(n, k, num_landscapes )
   sumsq_min_length = 0.0
   sumsq_ave_cost = 0.0
   sumsq_ave_dec_fitness = 0.0
-  path_summary_list = pmap(n_k->summary_landscape_paths(n_k[1], n_k[2], dist_cutoff=5), [(n,k) for _ = 1:num_landscapes])
+  path_summary_list = 
+      pmap(n_k->summary_landscape_paths(n_k[1], n_k[2], fit_diff_weight, peak_dist_cutoff=peak_dist_cutoff), [(n,k) for _ = 1:num_landscapes])
   for p in path_summary_list
     sum_ave_length += p.ave_length
     sum_min_length += p.min_length
@@ -247,7 +261,7 @@ Tests function least_cost_paths() by computing paths between peaks in both
 directions using a symmetric edge_cost function, and checking that lengths
 and costs are equal.
 """
-function test_paths(ls::NKLandscape, fits::Vector{Float64})
+function test_paths(ls::NKLandscape, fits::Vector{Float64},fit_diff_weight)
   bcc = sort(basinlists!(basins(ls,fits),ls),lt=(x,y)->x.peak_fitness<y.peak_fitness)
   if length(bcc) > 1
     for i = 1:length(bcc)
@@ -256,15 +270,11 @@ function test_paths(ls::NKLandscape, fits::Vector{Float64})
       for j = i+1:length(bcc)
         gj = bcc[j].gtype
         gj_set = Set{Genotype}([gj])
-        spw_ij = least_cost_paths(gi,gj_set,ls,fits,single_path=true,edge_cost_funct=edge_cost_sym)
-        spw_ji = least_cost_paths(gj,gi_set,ls,fits,single_path=true,edge_cost_funct=edge_cost_sym)
+        spw_ij = least_cost_paths(gi,gj_set,ls,fits,fit_diff_weight,single_path=true,edge_cost_funct=edge_cost_sym)
+        spw_ji = least_cost_paths(gj,gi_set,ls,fits,fit_diff_weight,single_path=true,edge_cost_funct=edge_cost_sym)
         @test  length(spw_ij[1].path) == length(spw_ji[1].path)
         @test_approx_eq spw_ij[1].cost spw_ji[1].cost
       end
     end
   end
 end
-
-ls = NKLandscape(5,3)
-fits = lsfits(ls)
-test_paths(ls,fits)
--- src/basins.jl
+++ src/basins.jl
@@ -13,67 +13,31 @@ type Basin
   peak_fitness::Float64
 end
 
-@doc """ function ranks(s::IntDisjointSets)
-
-returns the ranks array of the IntDisjointSets data structure s.  Not exported.
-"""
-function ranks(s::IntDisjointSets)
-  return s.internal.ranks
-end
-
-@doc """ function parents(s::IntDisjointSets)
-
-returns the parents array of the IntDisjointSets data structure s.  Not exported.
-"""
-function parents(s::IntDisjointSets)
-  return s.internal.parents
-end
-
-@doc """ function basins(ls::Landscape)
+@doc """ function basins(ls::NKLandscape; fits::Vector{Float64}=zeros(Float64,0))
 
 Returns an instance of `IntDisjointSets` that contains, as disjoint sets, the
 basins of attraction (relative to a greedy adaptive walk) present in the given landscape.
 The root of the basin of attraction is not necessarily the corresponding local optimum
 (but this property holds after basinlists!() is callled).
+The optional argument fits is an array of the fitnesses of the genotypes of the landscape.
 Should only be applied to NKLandscapes (rather than NKpLandscapes or NKqLandscapes at this time.
 """
-function basins(ls::NKLandscape)
+function basins(ls::NKLandscape, fits::Vector{Float64}=zeros(Float64,0))
   basin_sets = IntDisjointSets(ls.a^ls.n)
   for i = 0:(ls.a^ls.n - 1)
     g = Genotype(i,ls)
-    fg = fitness(g)
-    fittest = fittest_neighbor(g)
-    if fitness(fittest) <= fg
-      continue
-    else
-      union!(basin_sets,g.alleles+1,fittest.alleles+1)
-    end
-  end
-  return basin_sets
-end
-
-@doc """ function basins(ls::Landscape, fits::Vector{Float64})
-
-Returns an instance of `IntDisjointSets` that contains, as disjoint sets, the
-basins of attraction (relative to a greedy adaptive walk) present in the given landscape.
-The root of the basin of attraction is not necessarily the corresponding local optimum
-(but this property holds after basinlists!() is callled).
-Should only be applied to NKLandscapes (rather than NKpLandscapes or NKqLandscapes at this time.
-"""
-function basins(ls::NKLandscape, fits::Vector{Float64})
-  basin_sets = IntDisjointSets(ls.a^ls.n)
-  for i = 0:(ls.a^ls.n - 1)
-    g = Genotype(i,ls)
-    if fits != Void
+    if length(fits) > 0 
       fg = fits[i+1]
+      @assert fg == fitness(g)
     else
       fg = fitness(g)
     end
     fittest = fittest_neighbor(g)
     if length(fits) > 0
       f_fittest = fits[fittest.alleles+1]
+      @assert f_fittest == fitness(fittest)
     else
-      f_fittest = fitness(g)
+      f_fittest = fitness(fittest)
     end
     if f_fittest <= fg
       continue
@@ -148,16 +112,18 @@ function basinlists!(basins::IntDisjointSets,ls::NKLandscape)
   return basin_list
 end
 
-#=
-@doc """ function print_basin_summary(basin_lists)
+@doc """ function ranks(s::IntDisjointSets)
 
-Prints a summary of the list of basins returned by basinlists!().
-Example usage:  print_basin_summary(basinlists!(basins(ls),ls))  where ls is an NKLandscape
+returns the ranks array of the IntDisjointSets data structure s.  Not exported.
 """
-function print_basin_summary(basin_lists)
-  @printf("max_gen\tcount\tfitness\n")
-  for s in basin_lists
-    @printf("%s\t%4d\t%.6f\n","$(bits(s.gtype.alleles)[(end - s.gtype.landscape.n + 1):end])",s.count,s.peak_fitness)
-  end
+function ranks(s::IntDisjointSets)
+  return s.internal.ranks
 end
-=#
+
+@doc """ function parents(s::IntDisjointSets)
+
+returns the parents array of the IntDisjointSets data structure s.  Not exported.
+"""
+function parents(s::IntDisjointSets)
+  return s.internal.parents
+end
